3b9f3169add6c1a8b1a312d75ccc8c02d8ee516d
username <57055730+Jake-huen@users.noreply.github.com>
2023-11-16 14:46:50 +0900

    first commit

---
+++ src/main/java/com/example/outstagram/OutstagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.outstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutstagramApplication.class, args);
+    }
+
+}
